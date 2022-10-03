fd41fdbfa4d573f720fdcb0880f22a618d47b392
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-03 10:39:46 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
+import Head from "next/head"
 import Image from "next/image"
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Bundl | Boilerplate</title>
+    </Head>
     <div className="h-screen w-full bg-taupe pt-24">
       <div className="container mx-auto flex flex-col items-center">
         <Image 
@@ -11,9 +16,10 @@ export default function Home() {
         />
 
         <h1 className="font-primary font-bold text-center text-6xl max-w-xl mt-8">
-          Welcome to our startersproject.
+          Welcome to our boilerplate.
         </h1>
       </div>
     </div>
+    </>
   )
 }
